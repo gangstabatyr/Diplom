a244bad6ce2aca94a070af30c628e562fa6619b1
gangstabatyr <114847268+gangstabatyr@users.noreply.github.com>
2024-05-18 16:16:19 +0600

    Update cyber.py

--- cyber.py
+++ cyber.py
@@ -41,7 +41,7 @@ def main():
             st.success("You don't have risk of network threat")
         else:
             st.warning("WARNING!!! CODE RED!")
-      st.warning(f"WARNING!!! CODE RED!       {result}")
+      st.warning(result)
 
 if __name__ == '__main__':
     main()
